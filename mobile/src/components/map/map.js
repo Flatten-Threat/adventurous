a1d093e8875e7e2ca7a3811b413943bf332bc39c
Tritia Timmins <tritiatimmins@Tritias-MacBook-Pro.local>
2016-03-26 17:30:56 -0700

    adds basic navigation from map view to sign in page

--- mobile/src/components/map/map.js
+++ mobile/src/components/map/map.js
@@ -3,18 +3,58 @@ var React = require('react-native');
 var {
   Component,
   StyleSheet,
-  MapView
+  MapView,
+  View,
+  Text
 } = React;
+
+
+var Button = require('../common/button');
+
 
 module.exports = React.createClass({
   render: function(){
     return(
-      <MapView style={styles.map}></MapView>);
+        <View style={styles.container}>
+          <MapView style={styles.map}>
+          
+          </MapView>
+          
+          <View style={styles.buttonWrapper}>
+            <Button style={styles.button} text={'Sign up'} onPress={this.onPress} />
+            <Button style={styles.button} text={'Activities'} />
+          </View>
+        
+        </View>
+    );
+  },
+
+  //refer to this on line 20.
+  onPress: function() {
+    //navigate over to signup
+    //push into the navigator stack
+    this.props.navigator.push({name: 'signin'})
+    // this.props.navigator.push({name: 'activities'});
+
   }
+
 })
 
 var styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch'
+  },
+
   map: {
-    flex: 1
+    flex: 2,
+    marginTop: 30
+  },
+
+  buttonWrapper: {
+   flex: 1,
+   alignItems: 'center'
   }
+
 });

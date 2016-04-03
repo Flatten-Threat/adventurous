120e8a0e4a64b926cf4cfa91c10f069800a871b9
Jenna Vuong <jennavuong@gmail.com>
2016-04-02 20:50:35 -0700

    merged my code on top of tritia's latest in upstream master

--- mobile/src/components/map/map.js
+++ mobile/src/components/map/map.js
@@ -2,22 +2,27 @@ var React = require('react-native');
 var Icon_Restaurant = require('../images/icon_restaurant.png');
 var Icon_Shopping = require('../images/icon_shopping.png');
 var Icon_Pub = require('../images/icon_pub.png');
+var Button = require('../common/button.js'); // jenna moved here
+var MapPin = require('./mapPin.js'); // jenna
+var RightArrow = require('../images/icon_right_arrow.png');
 
 var {
   Component,
   StyleSheet,
   MapView,
   View,
-  Text
+  Text,
+  TouchableOpacity, // jenna
+  Image // jenna
 } = React;
-
-var Button = require('../common/button');
 
 module.exports = React.createClass({
 
   getInitialState: function() {
     return {
-      markers: [], // for pins
+      markers: [
+        this.createMapMarkers()
+        ],
       activityRootUrl: 'http://adventureus.herokuapp.com/api/activities' // jenna: needs to be prop, not sure where to put it
     };
   },
@@ -34,42 +39,9 @@ module.exports = React.createClass({
       })
       .then(function(json){
 
-        for (var i = 0; i < json.length; i++) {
-
-          // holder for region object
-          var holder = json[i].region;
-
-          // add title, description, image to region object
-          holder['title'] = json[i].title;
-          holder['subtitle'] = json[i].description;
-
-          console.log('json[i]: ', json[i]);
-
-          // assign pin image based on category
-          if ( json[i].category === 'Restaurant' ) {
-            holder['image'] = Icon_Restaurant;
-            console.log('inside restaurant! ');
-          }
-          else if ( json[i].category === 'Bar' ) {
-            holder['image'] = Icon_Pub;
-            console.log('inside bar! ');
-          }
-          else if ( json[i].category === 'Shopping' ) {
-            holder['image'] = Icon_Shopping;
-            console.log('inside shopping! ');
-          }
-
-          tempMarker.push(holder);
-        }
-
-        // set pins state
-        this.setState({
-          markers: tempMarker
-        });
-
+        console.log('this.state.markers: ', this.state.markers);
+        // this.createActivityMarkers(json);
       }.bind(this));
-
-      console.log('this.state.markers: ', this.state.markers);
   },
 
   render: function(){
@@ -78,48 +50,111 @@ module.exports = React.createClass({
 
         <View style={styles.container}>
           <MapView 
-          annotations={this.state.markers}
+          showsPointsOfInterest={false} // jenna
+          annotations={ this.state.markers }
           showsUserLocation={true}
           followUserLocation={true}
           style={styles.map}
           >
           </MapView>
 
-          <Button style={styles.button} text={'Add Activity'} onPress={this.showActivity} />
+          <Button style={styles.button} text={'Add Activity'} onPress={this.addActivity} />
           
         </View>
     );
   },
 
-  //when the user stops dragging the map this function is called
-  onRegionChangeComplete: function(region){
-    console.log(region);
+  // create 1 pin
+  createMapMarkers: function() {
+    return {
+      "title": "test pin state and navigation!",
+      "subtitle": "fun times ahead!",
+      "longitude": -122.268393,
+      "latitude": 37.880196,
+      "image": Icon_Shopping,
+      "rightCalloutView": (
+        <TouchableOpacity onPress={ this.navigateToActivityDetailView }>
+          <Image source={RightArrow} />
+        </TouchableOpacity>
+        )
+    }
+  },
+  // navigate to activity view
+  navigateToActivityDetailView: function() {
+    this.props.navigator.push({name: 'activities', passProps: {isNew: false}})
   },
 
-  //refer to this on line 20.
-  showActivity: function() {
+  addActivity: function() {
     //navigate over to signup
     //push into the navigator stack
     this.props.navigator.push({name: 'camera', passProps: {isNew: true}})
   }
 
-})
+  // // create array of markers
+  // createMarkerArray: function() {
+
+  //   var result = [];
+  //   result.push(createMapMarkers());
+
+  //   console.log('result: ', result);
+  //   return result;
+  // },
+
+  // createActivityMarkers: function(json) {
+
+  //   console.log('json: ', json);
+
+  //   // holder array for pins
+  //   var tempMarker = [];
+
+  //   for (var i = 0; i < json.length; i++) {
+
+  //     // holder for ea region object
+  //     var holder = json[i].region;
+
+  //     // add title, description, image to region object
+  //     holder['title'] = json[i].title;
+  //     holder['subtitle'] = json[i].description;
+
+  //     if ( json[i].category === 'Restaurant' ) {
+  //       holder['image'] = Icon_Restaurant;
+  //     }
+  //     else if ( json[i].category === 'Bar' ) {
+  //       holder['image'] = Icon_Pub;
+  //     }
+  //     else if ( json[i].category === 'Shopping' ) {
+  //       holder['image'] = Icon_Shopping;
+  //     }
+        
+  //     tempMarker.push(holder);
+  //   }
+
+  //   this.setState({
+  //     markers: tempMarker
+  //   });
+  // },
+
+}) // end of react class
 
 var styles = StyleSheet.create ({
+
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'stretch'
   },
-
   map: {
     flex: 2,
     marginTop: 30
   },
-
   buttonWrapper: {
    flex: 1,
    alignItems: 'center'
+  },
+  mapPin: {
+    borderWidth: 1,
+    borderRadius: 2,
+    backgroundColor: 'white'
   }
 
 });
---
+++ mobile/src/components/map/mapPin.js
@@ -0,0 +1,36 @@
+var React = require('react-native');
+var Icon_Shopping = require('../images/icon_shopping.png');
+
+var {
+    Image,
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity
+} = React;
+
+module.exports = React.createClass({
+
+  getInitialState: function() {
+    return {
+      isFirstLoad: false,
+      isSelected: false
+    };
+  },
+
+  render: function() {
+
+  },
+
+  // navigate to activity view
+  navigateToActDetailView: function() {
+    this.props.navigator.push({name: 'activities'})
+  }
+
+});
+
+var styles = StyleSheet.create({
+  mapPin: {
+
+  }
+})

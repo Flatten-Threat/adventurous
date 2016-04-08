04df225e0f9baa8313bcd58716210a85f73febbd
Jenna Vuong <jennavuong@gmail.com>
2016-04-07 19:44:06 -0700

    merged andy's code w navbar

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -3,6 +3,7 @@ var Map = require('./components/map/map');
 var Signin = require('./components/authentication/signin');
 var Activity = require('./components/activities/activity');
 var Camera = require('./components/camera/camera');
+var NavBar = require('./components/common/navbar');
 
 var {
   StyleSheet,
@@ -32,6 +33,7 @@ module.exports = React.createClass({
     return (
          <Navigator
          style={styles.container}
+         navigationBar={<NavBar />}
          //determines what first component is when navigator is first rendered
          initialRoute={{name: 'map'}}
          //contains functionality that tells the navigator how to render at any given time
